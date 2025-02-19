0f44b7d470caec72c2013a3f26da9c5f01811577
CyaCreates <tylerjsykora@gmail.com>
2025-02-18 21:55:25 -0800

    lets go

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>You Wish 1 2 3</h1>
+        <p>Welcome to the card game that will test your wit and luck!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
